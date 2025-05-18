2d9d8152cc362eff64ffe5432dab92f54e7272e4
AryaPeer <arya.peer@gmail.com>
2025-05-17 23:54:58 -0400

    Remove old files from bash script and rename backup -> archive

--- run.sh
+++ run.sh
@@ -2,16 +2,10 @@
 
 g++ -c src/main.cpp -o bin/main.o
 
-g++ -c src/dfds/binary-search-tree.cpp -o bin/static/binary-search-tree.o
-g++ -c src/dfds/avl-tree.cpp -o bin/static/avl-tree.o
-g++ -c src/dfds/circular-queue.cpp -o bin/static/circular-queue.o
-g++ -c src/dfds/doubly-linked-list.cpp -o bin/static/doubly-linked-list.o
-g++ -c src/dfds/dynamic-stack.cpp -o bin/static/dynamic-stack.o
-g++ -c src/dfds/sequential-list.cpp -o bin/static/sequential-list.o
 g++ -c src/dfds/dataframe.cpp -o bin/static/dataframe.o
 
-ar rcs bin/static/dfdsLib.a bin/static/binary-search-tree.o bin/static/avl-tree.o bin/static/circular-queue.o bin/static/doubly-linked-list.o bin/static/dynamic-stack.o bin/static/sequential-list.o bin/static/dataframe.o
+ar rcs bin/static/dataframeLib.a bin/static/dataframe.o
 
-g++ bin/main.o -Lbin/static -l:dfdsLib.a -o bin/execute_program
+g++ bin/main.o -Lbin/static -l:dataframeLib.a -o bin/execute_program
 
 echo "Compilation complete. Run ./bin/execute_program to execute the program."
